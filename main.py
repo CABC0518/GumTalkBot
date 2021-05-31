cae7bef68bbd9d71b0cccea19f18c32bda8fb6c1
takumi.aizawa1997@gmail.com <takumi.aizawa1997@gmail.com>
2021-05-31 21:26:33 +0300

    ?command and ?rule is now in text file

--- main.py
+++ main.py
@@ -3,19 +3,6 @@ import os
 import random
 from DB import database
 
-
-command_detail = """
-ガムトークbotで使えるコマンド一覧:
-
-?rule - ルールの説明
-?gum - 4つの話題を取得
-?dice - サイコロを振る
-?topics - 全ての話題を表示する
-"""
-
-rule = '「ガムトーク」は、めくったカードの中から、指定されたお題について話をするゲーム……というよりも、トーク用アイテム。話のオチはなくてオッケー、聞いた人は必ず「良い話や」と言うことになっているので、安心して話せます。"?gum"と入力してお題を引きましょう！サイコロは"?dice"で振れるよ！'
-
-
 def dice():
     return random.randint(1, 5)
 
@@ -57,11 +44,15 @@ async def on_message(message):
         await message.channel.send(dice_num)
 
     if message.content.startswith('?rule'):
-        await message.channel.send(rule)
+        with open('rule.txt', 'r') as file:
+            data = file.read()
+            await message.channel.send(data)
 
 
     if message.content.startswith('?command') or message.content.startswith('?help'):
-        await message.channel.send(command_detail)
+        with open('command.txt', 'r') as file:
+            data = file.read()
+            await message.channel.send(data)
 
     if message.content.startswith('?add '):
         topic = "'" + message.content.split()[1] + "'"
@@ -78,15 +69,6 @@ async def on_message(message):
     if message.content.startswith('?topics'):
         db = database(get_server_table(message.guild.id))
         result = db.get_all_topics()
-        # for index, topic in enumerate(result):
-        #     topic1, topic2, topic3, topic4 = topic
-        #     index = index + 1
-        #     topic =f"Topic {index}\n {topic1}、{topic2}、{topic3}、{topic4}\n"
-        #     await message.channel.send(topic)
-        # for topic in result:
-        #     await message.channel.send(topic)
-        print(result)
-        await message.channel.send(result)
 
 
 token = os.getenv("DISCORD_TOKEN")
